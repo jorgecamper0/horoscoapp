8e694d07e8baafd686e16bbdc40729dde99686a5
jorgecamper0 <jorgecamper0@hotmail.com>
2024-12-16 18:17:55 -0700

    primera animacion

--- app/src/main/java/com/jorgecamper0/horoscoapp/data/providers/HoroscopeProvider.kt
+++ app/src/main/java/com/jorgecamper0/horoscoapp/data/providers/HoroscopeProvider.kt
@@ -1,15 +1,25 @@
 package com.jorgecamper0.horoscoapp.data.providers
 
-import com.jorgecamper0.horoscoapp.R
 import com.jorgecamper0.horoscoapp.domain.model.HoroscopeInfo
-import com.jorgecamper0.horoscoapp.domain.model.HoroscopeInfo.*
+import com.jorgecamper0.horoscoapp.domain.model.HoroscopeInfo.Aquarius
+import com.jorgecamper0.horoscoapp.domain.model.HoroscopeInfo.Aries
+import com.jorgecamper0.horoscoapp.domain.model.HoroscopeInfo.Cancer
+import com.jorgecamper0.horoscoapp.domain.model.HoroscopeInfo.Capricorn
+import com.jorgecamper0.horoscoapp.domain.model.HoroscopeInfo.Gemini
+import com.jorgecamper0.horoscoapp.domain.model.HoroscopeInfo.Leo
+import com.jorgecamper0.horoscoapp.domain.model.HoroscopeInfo.Libra
+import com.jorgecamper0.horoscoapp.domain.model.HoroscopeInfo.Pisces
+import com.jorgecamper0.horoscoapp.domain.model.HoroscopeInfo.Sagittarius
+import com.jorgecamper0.horoscoapp.domain.model.HoroscopeInfo.Scorpio
+import com.jorgecamper0.horoscoapp.domain.model.HoroscopeInfo.Taurus
+import com.jorgecamper0.horoscoapp.domain.model.HoroscopeInfo.Virgo
 import javax.inject.Inject
 
 class HoroscopeProvider @Inject constructor() {
     fun getHoroscopes(): List<HoroscopeInfo> {
         return listOf(
-            Aries,
             Taurus,
+            Aries,
             Gemini,
             Cancer,
             Leo,
